49087b3428ef9acf5b4a4678ad2fcef8f23d45fd
Can Chen <canchen@me.com>
2017-10-05 21:27:02 -0400

    Convert jQuery code to Vanilla JS code

--- designs.js
+++ designs.js
@@ -1,42 +1,64 @@
-// global variable to pass current colorPicker value.
-let color = '#000000';
-
 // Select color input
-$('#colorPicker').on('change',function(){
-	color = $('#colorPicker').val();
-	$('#colorPicker_color').text(color); // show the current color picker value
+const colorPicker = document.getElementById('colorPicker');
+let color = colorPicker.value;
+
+colorPicker.addEventListener('change', function(){
+	color = colorPicker.value;
 })
 
 // Select size input
-$('#sizePicker').on('submit',function(event){
-	event.preventDefault();
+const sizePicker = document.getElementById('sizePicker');
 
-	let height = $('#input_height').val();
-	let width = $('#input_width').val();
+sizePicker.addEventListener('submit', function(event){
+	event.preventDefault();
+	let height = document.getElementById('input_height').value;
+	let width = document.getElementById('input_width').value;
 
 	clearGrid();
 	makeGrid(height, width);
 })
 
-
 // When size is submitted by the user, call makeGrid()
+const table = document.getElementById('pixel_canvas');
+
 function makeGrid(height, width) {
+	const tbody = document.createElement('tbody');
+
+	// tbody is required for JS to append node into table
+	table.appendChild(tbody);
+
 	// draw rows
 	for(let r = 0; r < height; r++) {
-		$('#pixel_canvas').append('<tr></tr>');	
+		tbody.appendChild( makeNewRow(width) );	
 	}
-	// draw columns
-	for(let c = 0; c < width; c++) {
-		$('tr').append('<td></td>');
-	}
+
 	// add event listener to all table cells
-	$('td').on('click',function(){
-		$(this).css('background-color',color);
+	const td = document.getElementsByTagName('td');
+
+	Array.prototype.forEach.call(td, function(td){
+		td.addEventListener('click', function(){
+			td.style.backgroundColor = color;
+		})
 	})
 
+	// populate one row with columns = width
+	function makeNewRow ( columns ) {
+		let tr = document.createElement('tr');
+
+		for(let c = 0; c < columns; c++) {
+			tr.appendChild( document.createElement('td') );
+		}
+
+		return tr	;
+	}
 }
 
 // When new size is submitted by the user, clear previous grid.
 function clearGrid() {
-	$('tbody').remove();
+	// if child not found => tbody === null
+	const tbody = table.firstChild;
+	// if tbody !== null, (null == false)
+	if (tbody) {
+		table.removeChild(tbody);
+	}
 }
